26fd7266db7fab957920216fea1c9625bca84058
LouisBioum <hbioum@yahoo.fr>
2023-09-07 00:20:59 +0100

    List options in sidebar

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,15 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import Head from 'next/head';
+import Sidebar from '@/components/Sidebar';
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <div>
-      <Head>
-        <title>Spotifyzer</title>
-      </Head>
-      <h1>Shopifizer: the best shopify clone ever</h1>
-
+    <div className='bg-black h-screen overflow-hidden'>
       <main>
-        {/* Sidebar */}
+        <Sidebar/>
         {/* Center */}
       </main>
       <div> {/* Player */} </div>
---
+++ pages/login.js
@@ -0,0 +1,8 @@
+function login() {
+  return (
+    <div>
+      <h1>This is a login page</h1>
+    </div>
+  );
+}
+export default login
