4e1fbed670cc69759b4fe56c5e5d22d8a340d5c4
LouisBioum <hbioum@yahoo.fr>
2023-09-10 00:26:52 +0100

    Styled the header

--- pages/index.js
+++ pages/index.js
@@ -2,15 +2,16 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import Head from 'next/head';
 import Sidebar from '@/components/Sidebar';
+import Center from '@/components/Center';
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <div className='bg-black h-screen overflow-hidden'>
-      <main>
+      <main className='flex'>
         <Sidebar/>
-        {/* Center */}
+        <Center />
       </main>
       <div> {/* Player */} </div>
     </div>
