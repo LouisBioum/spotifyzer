58c0e2cb723c093416e976e53003cd72ae4b9cd0
LouisBioum <hbioum@yahoo.fr>
2023-09-11 05:43:56 +0100

    show duration, style track list, signout avatar

--- components/Song.js
+++ components/Song.js
@@ -1,11 +1,12 @@
 import useSpotify from "@/hooks/useSpotify"
+import { millisToMinutesseconds } from "@/lib/time";
 
 function Song({ order, track }) {
     const spotifyApi = useSpotify()
-    console.log(track)
+    console.log(track.track)
 
   return (
-    <div className="grid grid-cols-2">
+    <div className="grid grid-cols-2 text-gray-500 py-4 hover:bg-gray-900 rounded-lg cursor-pointer">
       <div className="flex items-center space-x-4">
         <p>{order + 1}</p>
         <img
@@ -14,14 +15,14 @@ function Song({ order, track }) {
           alt=""
         />
         <div>
-          <p>{track.track.name}</p>
-          <p>{track.track.artists[0].name}</p>
+          <p className="w-36 lg:w-64 truncate text-white">{track.track.name}</p>
+          <p className="w-40">{track.track.artists[0].name}</p>
         </div>
       </div>
 
       <div className="flex items-center justify-between ml-auto md:ml-0">
-        <p className="hidden md:inline">{track.track.album.name}</p>
-        <p>duration</p>
+        <p className="hidden md:inline w-40">{track.track.album.name}</p>
+        <p>{millisToMinutesseconds(track.track.duration_ms)}</p>
       </div>
     </div>
   );
