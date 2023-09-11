1d526bc6d0dc454e13153173dcc859f551565501
LouisBioum <hbioum@yahoo.fr>
2023-09-11 05:55:51 +0100

    Play track remotely on other spotify device

--- components/Song.js
+++ components/Song.js
@@ -1,12 +1,23 @@
+import { currentTrackIdState, isPlayingState } from "@/atoms/songAtom";
 import useSpotify from "@/hooks/useSpotify"
 import { millisToMinutesseconds } from "@/lib/time";
+import { useRecoilState } from "recoil";
 
 function Song({ order, track }) {
     const spotifyApi = useSpotify()
+    const [currentTrackId, setCurrentTrackId] = useRecoilState(currentTrackIdState)
+    const [isPlayinf, setIsPlaying] = useRecoilState(isPlayingState)
+
+    const playSong = () => {
+        setCurrentTrackId(track.track.id)
+        setIsPlaying(true)
+        spotifyApi.play({uris: [track.track.uri]})
+    }
     console.log(track.track)
 
   return (
-    <div className="grid grid-cols-2 text-gray-500 py-4 hover:bg-gray-900 rounded-lg cursor-pointer">
+      <div className="grid grid-cols-2 text-gray-500 py-4 hover:bg-gray-900 rounded-lg cursor-pointer"
+      onClick={playSong }>
       <div className="flex items-center space-x-4">
         <p>{order + 1}</p>
         <img
