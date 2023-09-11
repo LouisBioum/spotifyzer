bae4b63a80ac216ca5479a6f38291abb757a607b
LouisBioum <hbioum@yahoo.fr>
2023-09-11 10:41:08 +0100

    Updating the app with currently playiing song

--- components/Player.js
+++ components/Player.js
@@ -2,7 +2,7 @@ import { currentTrackIdState, isPlayingState } from "@/atoms/songAtom"
 import useSongInfo from "@/hooks/useSongInfo"
 import useSpotify from "@/hooks/useSpotify"
 import { useSession } from "next-auth/react"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { useRecoilState } from "recoil"
 
 function Player() {
@@ -12,7 +12,25 @@ function Player() {
   const [isPlaying, setIsPlaying] = useRecoilState(isPlayingState)
   const [volume, setVolume] = useState(50)
   const songInfo = useSongInfo()
-  console.log(songInfo)
+  // console.log(songInfo)
+
+  const fetchCurrentSong = () => {
+    if (!songInfo) {
+      spotifyApi.getMyCurrentPlayingTrack().then((data) => {
+        console.log("Now playing: ", data.body?.item)
+        setCurrentTrackId(data.body?.item.id)
+      })
+
+      spotifyApi.getMyCurrentPlaybackState().then((data) => {
+        setIsPlaying(data.body?.isPlaying)
+      })
+    }
+  }
+
+  useEffect(() => {
+    fetchCurrentSong()
+    setVolume(50)
+  }, [currentTrackId, spotifyApi, session])
   return (
     <div>
       {/* Left */}
