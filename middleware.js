fefffb8b59032abdeb5b074c17fdd97c657aa4dc
LouisBioum <hbioum@yahoo.fr>
2023-09-12 23:56:56 +0100

    Clarified login requirement

--- middleware.js
+++ middleware.js
@@ -2,7 +2,7 @@ import { getToken } from "next-auth/jwt";
 import { NextResponse } from "next/server";
 
 export async function middleware(req) {
-  // Token will exist if user is logged in
+  // Token will exist if user is logged in spotify
   const token = await getToken({ req, secret: process.env.JWT_SECRET });
 
   const { pathname } = req.nextUrl;
